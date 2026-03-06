9d2d95278e4dcbcd1e0fc7baae9a96cefd213c98
Jinyuan Zhou <zhou.jinyuan@gmail.com>
2026-03-06 13:14:24 -0800

    base code

---
+++ challenge/sort.py
@@ -0,0 +1,32 @@
+"""Package type based on dimensions and mass.
+
+Determines whether a package is STANDARD, SPECIAL, or REJECTED based
+on its volume and mass.
+"""
+
+
+class Package:
+    def __init__(self, width, height, length, mass):
+        self.width = width
+        self.height = height
+        self.length = length
+        self.mass = mass
+        self.stack = self.get_stack()
+
+    def is_bulky(self):
+        return self.width * self.height * self.length >= 1000000 or self.width >= 150 or self.height >= 150 or self.length >= 150
+
+    def is_heavy(self):
+        return self.mass >= 20
+
+    def get_stack(self):
+        if self.is_bulky() and self.is_heavy():
+            return "REJECTED"
+        elif self.is_bulky() or self.is_heavy():
+            return "SPECIAL"
+        else:
+            return "STANDARD"
+
+def sort(width, height, length, mass):
+    package = Package(width, height, length, mass)
+    return package.stack
